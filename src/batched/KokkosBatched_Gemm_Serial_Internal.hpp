7a706c0842218495d53595e4c67ed0097860623b
Evan Harvey <eharvey@sandia.gov>
2021-04-07 09:03:36 -0600

    Revert "perf_test/blas: Changes to Dot for OPT2"
    This reverts commit 3db98e482d7f668672b0ff7658e896b8313c8774.

--- src/batched/KokkosBatched_Gemm_Serial_Internal.hpp
+++ src/batched/KokkosBatched_Gemm_Serial_Internal.hpp
@@ -89,9 +89,10 @@ namespace KokkosBatched {
     // C (m x n), A(m x k), B(k x n)
     //printf("as0=%d,as1=%d,bs0=%d,bs1=%d,cs0=%d,cs1=%d\n",as0,as1,bs0,bs1,cs0,cs1);
     int i = 0;
-    C[0] *= beta;//alpha * A[i] * B[i];
+    ScalarType dp = C[0] * beta;//alpha * A[i] * B[i];
     for (i = 0; i < k; i++)
-      C[0] += alpha * A[i*as0] * B[i*bs0];
+      dp += alpha * A[i*as0] * B[i*bs0];
+    C[0] = dp;
     return 0;
   }
 
